f0358d307ec31e670210db884ae4965b68e854f3
jmosesman <jmosesman@ou.edu>
2015-03-08 14:05:49 -0500

    Remove adapter code

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,5 @@
 import DS from 'ember-data';
-// import config from '../config/environment';
 
 export default DS.ActiveModelAdapter.extend({
-  // host: 'http://localhost:3000',
   namespace: "api"
 });
